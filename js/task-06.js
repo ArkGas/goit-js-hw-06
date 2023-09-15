b9d5c29e3a7066f16a5d792d1eca32b881c4df26
ArkGas <130868259+ArkGas@users.noreply.github.com>
2023-09-16 01:43:17 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,10 @@ const validationInput = document.querySelector("#validation-input");
 const attributeValue = validationInput.getAttribute("data-length");
 
 const doValidation = () => {
-  if (validationInput.value.length < attributeValue) {
+  if (
+    validationInput.value.length < attributeValue ||
+    validationInput.value.length > attributeValue
+  ) {
     validationInput.classList.add("invalid");
     validationInput.classList.remove("valid");
   } else {
